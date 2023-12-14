acf58bd0da29eaccb8f2ce0268d27620b665a4b6
Tyler <tyler@TylerdeMacBook-Pro.local>
2023-12-14 22:25:56 +0800

    second commit

--- models/note.go
+++ models/note.go
@@ -7,10 +7,40 @@ import (
 )
 
 type Note struct {
-	ID uint `json:"id" gorm:"primary_key"`
-	Name string `json:"name" gorm:"size:255"` 
-	Content string `json:"content" gorm:"type:text"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdateAt time.Time `json:"updated_at" gorm:"index"`
-	DeleteAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	ID        uint64 `gorm:"primaryKey"`
+	Name      string `gorm:"size:255"`
+	Content   string `gorm:"type:text"`
+	UserID    uint64 `gorm:"index"`
+	CreatedAt time.Time
+	UpdatedAt time.Time      `gorm:"index"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func NotesAll(user *User) *[]Note {
+	var notes []Note
+	DB.Where("deleted_at is NULL and user_id = ?", user.ID).Order("updated_at desc").Find(&notes)
+	return &notes
+}
+
+func NoteCreate(user *User, name string, content string) *Note {
+	entry := Note{Name: name, Content: content, UserID: user.ID}
+	DB.Create(&entry)
+	return &entry
+}
+
+func NotesFind(user *User, id uint64) *Note {
+	var note Note
+	DB.Where("id = ? and user_id = ?", id, user.ID).First(&note)
+	return &note
+}
+
+func (note *Note) Update(name string, content string) {
+	note.Name = name
+	note.Content = content
+	DB.Save(note)
+}
+
+func NotesMarkDelete(user *User, id uint64) {
+	// UPDATE notes SET deleted_at=<Current Time> WHERE id = <id> and user_id = <user_id>
+	DB.Where("id = ? and user_id = ?", id, user.ID).Delete(&Note{})
 }
--- models/setup.go
+++ models/setup.go
@@ -1,23 +1,23 @@
 package models
 
 import (
-	"log"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
-var DB *gorm.DB
-func ConnectDB() {
-	// Connect to DB
-	database, err := gorm.Open(mysql.Open("root:B0115588f!@tcp(127.0.0.1:3306)/notes?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect to database!")
-	}
-	log.Println("Database connected!")
-	DB = database
 
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	database, err := gorm.Open(mysql.Open("root:B0115588f!@tcp(127.0.0.1:3306)/notes?charset=utf8&parseTime=true"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to databse!")
+	}
+
+	DB = database
 }
 
 func DBMigrate() {
 	DB.AutoMigrate(&Note{})
+	DB.AutoMigrate(&User{})
 }
